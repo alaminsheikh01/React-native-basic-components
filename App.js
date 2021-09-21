417ea424f345925f3c9f0f59175cc6f6cd0fa1ca
alamin <alamin@gmail.com>
2021-09-22 00:10:03 +0600

    add and show data

--- App.js
+++ App.js
@@ -1,41 +1,70 @@
-import React from 'react';
-import {Text, View, Button, TextInput} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 
 const App = () => {
+  const [enterGoal, setEnterGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = enteredText => {
+    setEnterGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enterGoal]);
+    setEnterGoal('');
+  };
+
   return (
-    <View style={{padding: 50, flexDirection: 'row'}}>
-      <View
-        style={{
-          backgroundColor: 'red',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text>1</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputcontainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Course Goal"
+          onChangeText={goalInputHandler}
+          value={enterGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View
-        style={{
-          backgroundColor: 'green',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'yellow',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text>3</Text>
-      </View>
+      <ScrollView>
+        {courseGoals.map(goal => (
+          <Text style={styles.listItem} key={goal}>
+            {goal}
+          </Text>
+        ))}
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputcontainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%',
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+    marginVertical: 10,
+  },
+});
 
 export default App;
